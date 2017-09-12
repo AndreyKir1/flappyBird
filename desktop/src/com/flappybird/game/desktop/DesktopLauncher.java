c1a8d33927cc0531d7b1848f03d2b5c9beda3553
AndreyKir1 <smite@i.ua>
2017-09-12 13:39:59 +0200

    flappy 1

---
+++ desktop/src/com/flappybird/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.flappybird.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.flappybird.game.flappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new flappyBird(), config);
+	}
+}
